ffa5234580e4abb5926ff3da958b7f1be30e1126
miseong shin <miseong1028@gmail.com>
2021-04-16 14:01:26 +0900

    Crate Package https://start.spring.io/

---
+++ src/main/java/com/web/javademo1028web/JavaDemo1028webApplication.java
@@ -0,0 +1,13 @@
+package com.web.javademo1028web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDemo1028webApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDemo1028webApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/javademo1028web/JavaDemo1028webApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.javademo1028web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDemo1028webApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
